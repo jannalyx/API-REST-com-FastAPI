c3441fb337904598a42e62422434e4b2043821d9
Jana <janainapcj44@gmail.com>
2025-05-06 22:18:34 -0300

    refinamento do código

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ from app.routers.usuarios_router import router as usuarios_router
 from app.routers.pedidos_router import router as pedidos_router
 from app.routers.exportar_router import router as exportar_router  
 
-app = FastAPI()
+app = FastAPI(
+    title="MyBooks",  
+    description="**Sistema de venda de livros.**",  
+    version="0.1.0",  
+)
 
 app.include_router(livros_router)
 app.include_router(usuarios_router)
 app.include_router(pedidos_router)
-app.include_router(exportar_router)  
+app.include_router(exportar_router)
